282057e4d3179ab9be5843e68fc308129bad7d38
DV-Hung <dvhung166@gmail.com>
2026-04-27 17:20:17 +0700

    chore(fe): setup application configuration and i18n localization

---
+++ techstore_fe/src/i18n/i18n.ts
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import enLocale from '../locales/en.json';
+import viLocale from '../locales/vi.json';
+
+const resources = {
+  en: {
+    translation: enLocale,
+  },
+  vi: {
+    translation: viLocale,
+  },
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: localStorage.getItem('language') || 'vi', // Default to Vietnamese
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
